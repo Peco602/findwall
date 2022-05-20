cb5edc1350a8f63fffcf2fb7997db84673fc4753
Peco602 <giovanni1.pecoraro@protonmail.com>
2022-05-20 14:20:28 +0200

    update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 ![TCP](https://github.com/Peco602/findwall/actions/workflows/tcp.yml/badge.svg)
 ![UDP](https://github.com/Peco602/findwall/actions/workflows/udp.yml/badge.svg)
+
+<img src="demo.gif" width="100%"/>
+
+python findwall.py --ssh-host 172.17.0.2 --ssh-port 22 --ssh-username test --ssh-password test --ports 8000-8010 --threads 3
+
+sudo docker run --rm -v $PWD:/data asciinema/asciicast2gif -t solarized-dark demo.cast demo.gif
---
+++ test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+echo "[!] Creating virtual environment"
+rm -rf venv
+virtualenv -v venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+echo "[!] Creating target"
+docker run --cap-add=NET_ADMIN --name findwall_target --publish-all -d ghcr.io/s1ntaxe770r/image:latest
+docker exec -t findwall_target apt install -y netcat iptables
+
+echo "[!] Adding firewall rules to target"
+docker exec -t findwall_target iptables -A INPUT -p tcp --destination-port 8001 -j DROP
+docker exec -t findwall_target iptables -A INPUT -p tcp --destination-port 8005 -j DROP
+docker exec -t findwall_target iptables -A INPUT -p udp --destination-port 8002 -j DROP
+
+echo "[!] TCP scan"
+python findwall.py --ssh-host $(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' findwall_target) --ssh-port 22 --ssh-username test --ssh-password test --ports 8000-8010 --threads 3
+
+echo "[!] UDP scan"
+python findwall.py --ssh-host $(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' findwall_target) --ssh-port 22 --ssh-username test --ssh-password test --ports 8000-8010 --udp --threads 3
+
+echo "[!] Clean-up"
+docker rm findwall_target --force
+deactivate
